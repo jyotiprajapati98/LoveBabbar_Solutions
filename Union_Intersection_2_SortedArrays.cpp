a2bf3492dac63490f1f7f5c510365d7eab2941a2
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2023-06-04 23:54:09 +0530

    Update Union_Intersection_2_SortedArrays.cpp

--- Union_Intersection_2_SortedArrays.cpp
+++ Union_Intersection_2_SortedArrays.cpp
@@ -1,60 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-void arrayPrinting(int arr[],int n){
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-}
-
-void Union(int arr1[], int arr2[], int m, int n)
-{
-    int i = 0, j = 0;
-    while (i < m && j < n) {
-        if (arr1[i] < arr2[j])
-            cout << arr1[i++] << " ";
- 
-        else if (arr2[j] < arr1[i])
-            cout << arr2[j++] << " ";
- 
-        else {
-            cout << arr2[j++] << " ";
+void unionSet(int a[], int b[], int n, int m){
+    int i=0, j=0;
+    while(i<n && j<m){
+        if(a[i]<b[j]){
+            cout<<a[i++]<<" ";
+        }else if(b[j]<a[i]){
+            cout<<b[j++]<<" ";
+        }
+        else{
+            cout<<b[j++]<<" ";
             i++;
         }
     }
- 
-    /* Print remaining elements of the larger array */
-    while (i < m)
-        cout << arr1[i++] << " ";
- 
-    while (j < n)
-        cout << arr2[j++] << " ";
+    
+    //print array a
+    while(i<n){
+        cout<<a[i++]<<" ";
+    }
+    
+    while(j<m){
+        cout<<b[j++]<<" ";
+    }
 }
- 
-int main(){
-	int s1,s2;
-	cout<<"Enter the array 1 size -->";
-	cin>>s1;
-	int arr1[s1];
-	for(int i=0;i<s1;i++){
-		cout<<"value "<<i << "->";
-		cin>>arr1[i];
-	}
-	
-	cout<<"\nEnter the array 2 size -->";
-	cin>>s2;
-	int arr2[s2];
-	for(int i=0;i<s2;i++){
-		cout<<"value "<<i << "->";
-		cin>>arr2[i];
-	}
-	cout<<"\nArray 1 values"<<"\n";
-	arrayPrinting(arr1,s1);
-	cout<<"\nArray 2 values"<<"\n";
-	arrayPrinting(arr2,s2);
-	
-	cout<<"\nUnion of arrays"<<"\n";
-	Union(arr1, arr2, s1,s2);
-	return 0;
+
+//intersection
+void intersection(int a[], int b[], int n, int m){
+    int i=0, j=0;
+    while(i<n && j<m){
+        if(a[i]<b[j]){
+            i++;
+        }else if(b[j]<a[i]){
+            j++;
+        }else{
+            cout<<b[j]<<" ";
+            i++;
+            j++;
+        }
+    }
+}
+int main()
+{
+    int a[] = {1,2,3};
+    int b[] = {1,2,3};
+    
+    int n = sizeof(a)/sizeof(a[0]);
+    int m = sizeof(b)/sizeof(b[0]);
+    
+    //call union method
+    cout<<"Union of set is -> ";
+    unionSet(a, b, n, m);
+    
+    cout<<"\nIntersection of set is -> ";
+    intersection(a, b, n, m);
+    return 0;
 }
