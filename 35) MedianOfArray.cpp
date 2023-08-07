271559d810e55113685b9f24dbee6ca2449ef0da
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2023-08-08 00:02:30 +0530

    Create 35) MedianOfArray.cpp

---
+++ 35) MedianOfArray.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v){
+		    sort(v.begin(), v.end());
+		    int n = v.size();
+		    int median = 0;
+		    
+		    if(n%2==0){
+		        median = (v[n/2-1] + v[n/2])/2;
+		    }else{
+		        median = v[n/2];
+		    }
+		    
+		    return median;
+		}
+		
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
