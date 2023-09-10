47a49c3048f0cd506e14c1dce3b27cb71f483a34
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2023-09-11 00:07:35 +0530

    Update 60) EDIT Distance.cpp

--- 60) EDIT Distance.cpp
+++ 60) EDIT Distance.cpp
@@ -7,20 +7,41 @@ using namespace std;
 class Solution{
     public:
     int editDistance(string s, string t){
-        int n = s.size(), m = t.size(), i = 0, j = 0;
-        int dp[n+1][m+1];
-        for(int i = 0;i<=n;i++)    dp[i][0] = i;
-        for(int i = 0;i<=m;i++)    dp[0][i] = i;
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j = 1;j<=m;j++)
-            {
-              if(s[i-1]== t[j-1])   dp[i][j] = dp[i-1][j-1];
-                
-              else  dp[i][j] = 1+  min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));  
+        //calculate the size of both the string 
+        int n = s.size(), m = t.size();
+        
+        //initialize two variables
+        int i = 0, j = 0;
+        
+        //initialize array with size [N+1][M+1]
+        int arr[n+1][m+1];
+        
+        //traverse over the first string
+        for(int i = 0;i<=n;i++){
+            //store the first string charecters in 0th place, 2nd palce will be empty
+            arr[i][0] = i; //
+        }
+        
+        //similary for 2nd string
+        for(int i = 0;i<=m;i++){   
+            arr[0][i] = i;
+        }
+        
+        //Now check the each charecter
+        for(int i = 1;i<=n;i++){
+            //for string 1
+            for(int j = 1;j<=m;j++){
+            //for string 2
+            
+            //check with if condition
+                if(s[i-1]== t[j-1]){
+                  arr[i][j] = arr[i-1][j-1];
+                }else{
+                  arr[i][j] = 1 + min(arr[i-1][j], min(arr[i][j-1], arr[i-1][j-1]));
+              }
             }
         }
-       return dp[n][m];
+       return arr[n][m];
     }
 };
 
