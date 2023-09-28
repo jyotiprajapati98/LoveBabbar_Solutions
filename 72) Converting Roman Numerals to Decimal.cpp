b97e60e72a92e3034c419c9f7a96d58c821f963b
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2023-09-28 23:55:36 +0530

    Update 72) Converting Roman Numerals to Decimal.cpp

--- 72) Converting Roman Numerals to Decimal.cpp
+++ 72) Converting Roman Numerals to Decimal.cpp
@@ -1,6 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
+//using unordered map
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+       //using unordered map
+       unordered_map<char, int> unmap;
+       unmap['I'] = 1;
+       unmap['V'] = 5;
+       unmap['X'] = 10;
+       unmap['L'] = 50;
+       unmap['C'] = 100;
+       unmap['D'] = 500;
+       unmap['M'] = 1000;
+       
+       //check the max values
+       int max = unmap[str[str.length()-1]];
+       int ans = 0;
+       
+       for(int i=str.length()-1; i>=0; i--){
+           int number = unmap[str[i]];
+           
+           if(number>=max){
+               ans = number + ans;
+               max = number;
+           }else{
+               ans -= number;
+           }
+       }
+       
+       return ans;
+    }
+*/
 int romanToDecimal(string s){
 	// declare two variables first
 	// will calculate the number
