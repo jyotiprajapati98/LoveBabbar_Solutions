f13ed33b7f0f3912f943a7b185ccf4736ef29c4d
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2023-10-05 00:34:59 +0530

    Update 79) Smallest distinct window.cpp

--- 79) Smallest distinct window.cpp
+++ 79) Smallest distinct window.cpp
@@ -1,10 +1,67 @@
-class Solution{
-    public:
-    int findSubString(string str){
-        unordered_set<char> us;
-        for(int i=0; i<str.size(); i++){
-            us.insert(i);
-        }  
-        return us.size();
+using namespace std;
+#include <bits/stdc++.h>
+int const MAX_CHAR = 256;
+
+// Mehtod to find the smallest distinct Window for substring
+string SubString(string str){
+    int N = str.length();
+    
+    //base case
+    if(N<=1){
+        return str;
     }
-};
+    
+    int dist = 0;
+    bool visited[MAX_CHAR] = {false}; //initialize with false
+    
+    //count the distict charecters
+    for(int i=0; i<N; i++){
+        if(visited[str[i]] == false){
+            visited[str[i]] = true;
+            dist++;
+        }
+    }//end of for loop
+    
+    int start = 0, start_index = -1, min_len = INT_MAX;
+    
+    int cnt = 0;
+    int curr_cnt[MAX_CHAR] = {0};
+    
+    for(int j=0; j<N; j++){
+        curr_cnt[str[j]]++;
+        
+        if(curr_cnt[str[j]] == 1){
+            cnt++;
+        }
+        
+        if(cnt == dist){
+            while(curr_cnt[str[start]] > 1){
+                if(curr_cnt[str[start]] > 1){
+                    curr_cnt[str[start]]--;
+                }
+                start++;
+            }
+            
+            //update the Window size
+            int wlen = j - start + 1;
+            if(min_len > wlen){
+                min_len = wlen;
+                start_index = start;
+            }
+            
+        }
+    }
+    
+    return str.substr(start_index, min_len);
+}
+
+
+//main method
+int main()
+{
+	string str = "aabcbcdbca";
+	cout << "Smallest window containing all distinct"
+			" characters is: "
+		<< SubString(str);
+	return 0;
+}
