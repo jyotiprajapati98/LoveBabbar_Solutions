f1b4d53e8a2a52145fbdade6f6397d67212ac783
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2023-10-27 23:37:54 +0530

    Create 92)Search in a rotated sorted array.cpp

---
+++ 92)Search in a rotated sorted array.cpp
@@ -0,0 +1,17 @@
+//simple code
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int index = -1;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target){
+                index = i;
+            }
+        }
+
+        return index;
+    }
+};
+
+//medium code
