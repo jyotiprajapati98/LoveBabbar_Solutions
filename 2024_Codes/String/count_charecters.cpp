61be5bc9e2f9d5ceba1266232b7693ddf374cf38
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2024-05-09 23:38:42 +0530

    Create count_charecters.cpp

---
+++ 2024_Codes/String/count_charecters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include<bits/stdc++.h>
+int main()
+{
+    string name;
+    cout<<"Type a String : ";
+    cin>>name;
+    cout<<"\nYour name is : "<<name;
+
+    return 0;
+}
